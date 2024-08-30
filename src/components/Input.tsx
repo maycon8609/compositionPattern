2af9e96d0bfaa6c011a37ebb7ce09c8116041be1
maycon silva <maycon8609@gmail.com>
2024-08-30 15:27:16 -0300

    refactor: update components

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,10 +2,18 @@ import { FC } from "react";
 
 import { InputProps } from "./types";
 
-import "./Input.css";
+import { cn } from "../lib/utils";
 
-export const Input: FC<InputProps> = (props) => {
-  return <input {...props} />
+export const Input: FC<InputProps> = ({className, ...props}) => {
+  return (
+  <input
+    className={cn(
+      "border-none bg-gray-50 text-gray-900 w-full my-2 mx-0 focus-within:outline-none placeholder:text-gray-400",
+      className
+    )}
+    {...props}
+  />
+)
 };
 
 export default Input;
--- src/components/InputRoot.tsx
+++ src/components/InputRoot.tsx
@@ -2,8 +2,16 @@ import { FC } from "react"
 
 import { InputRootProps } from "./types"
 
-import "./InputRoot.css"
+import { cn } from "../lib/utils"
 
-export const InputRoot: FC<InputRootProps> = (props) => {
-  return <div className="input-root" {...props} />
+export const InputRoot: FC<InputRootProps> = ({ className, ...props}) => {
+  return (
+  <div 
+    className={cn(
+      "flex content-between p-2 bg-gray-50 rounded-md  focus-within:outline-2 focus-within:outline-gray-500 focus-within:outline-offset-2",
+      className
+    )}
+    {...props}
+  />
+)
 }
